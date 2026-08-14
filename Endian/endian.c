082269a02c4a3079a72129d1e33e545f97d85998
oonuki_r <r202203767lt@jindai.jp>
2026-08-14 17:19:40 +0900

    create endian.c

--- Endian/endian.c
+++ Endian/endian.c
@@ -2,15 +2,21 @@
 #include <string.h>
 #include <arpa/inet.h>
 
+void print_endian(int data) {
+    char *p = (char *)&data;
+    for(int i = 0; i < sizeof(data); i++) {
+        printf("+%d : %02X\n", i, p[i]);
+    }
+}
+
 int main(void) {
     int data = 0x12345678;
 
     data = htonl(data);
-    char *p = (char *)&data;
-
-    for(int i = 0; i < sizeof(data); i++) {
-        printf("+%d : %02X\n", i, p[i]);
-    }
+    print_endian(data);
+    printf("\n");
+    data = ntohl(data);
+    print_endian(data);
 
     return 0;
 }
