764b40f2664591589938b55565132789ac26b6e1
oonuki_r <r202203767lt@jindai.jp>
2026-08-14 14:25:58 +0900

    make socket

--- TCP-IP/socket.c
+++ TCP-IP/socket.c
@@ -5,11 +5,14 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <string.h>
 
-int main(void) {
-    int socket_make;
+#define MAX_MESSAGE (64)
+
+int socket_make;
+
+void server_connect() {
     struct sockaddr_in server_adder;
-
     socket_make = socket(AF_INET, SOCK_STREAM, 0);
     if(socket_make < 0) {
         printf("ソケット作成に失敗\n");
@@ -17,8 +20,8 @@ int main(void) {
     }
 
     server_adder.sin_family = AF_INET;
-    server_adder.sin_port = htons(8080);
-    server_adder.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server_adder.sin_port = htons(35279);//ポート番号
+    server_adder.sin_addr.s_addr = inet_addr("127.0.0.1");//IPアドレス
 
     if(connect(socket_make, (struct sockaddr *)&server_adder, sizeof(server_adder)) < 0) {
         printf("接続に失敗\n");
@@ -27,6 +30,22 @@ int main(void) {
     }
 
     printf("サーバーに接続\n");
+}
+
+int main(void) {
+    char message[] =
+    "GET / HTTP/1.1\r\n"
+    "Host: localhost\r\n"
+    "\r\n";
+    char receive[MAX_MESSAGE];
+
+    server_connect();
+    send(socket_make, message, strlen(message), 0);
+
+    recv(socket_make, receive, sizeof(receive), 0);
+    printf("受信\n");
+    printf("%s\n", receive);
+
     close(socket_make);
     return 0;
 }
