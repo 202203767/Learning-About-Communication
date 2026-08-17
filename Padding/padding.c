3e1b26db3ca266cc4ad545e8ddab6cfaff295639
oonuki_r <r202203767lt@jindai.jp>
2026-08-17 14:11:58 +0900

    パディングについての説明を追加

--- Padding/padding.c
+++ Padding/padding.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
-#define MAX_DATA 64
+#define MAX_DATA 8
 
 typedef struct data {
-    int id;
-    char data[MAX_DATA];
+    unsigned char id;
+    int data[MAX_DATA];
 } data;
 
 int main(void) {
     data sample_data;
-    printf("%zu\n", sizeof(sample_data));
-    printf("%zu\n", sizeof(sample_data.id));
-    printf("%zu\n", sizeof(sample_data.data));
+    
+    printf("構造体全体のバイト数は%zu\n", sizeof(sample_data));
+    printf("%zu %p\n", sizeof(sample_data.id), &sample_data.id);
+    printf("%zu %p\n", sizeof(sample_data.data), &sample_data.data);
 
     return 0;
 }
